5bd36b8a39eaa97cbcf3ce6283d7788d844cdcfd
MatLamber <matixlambaer@gmail.com>
2024-07-16 15:13:30 -0400

    StartPointForNewCameraBranch

---
+++ Assets/_PortalTowers/Scripts/Uitility/CubFollower.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubFollower : MonoBehaviour
+{
+    [SerializeField] private Transform target;
+
+    private void Update()
+    {
+        transform.position =
+            new Vector3(target.transform.position.x, target.transform.position.y, transform.position.z);
+    }
+}
